9b085d7efab257b8ce32d0142938ea33a5f4449e
hundessa <hundserb@gmail.com>
2024-02-20 00:58:59 +0300

    initial commit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,12 +4,4 @@
 
 def is_same_class(obj, a_class):
     """checking if obj is exactly an instance of a_class"""
-    return isinstance(obj, a_class)
-
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
+    return type(obj) == a_class
